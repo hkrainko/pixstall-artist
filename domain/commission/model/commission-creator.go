39a9189bfe8ac971cad9223c56fc43118bc37ce1
hkrainko <>
2021-02-21 22:11:58 +0800

    no message

--- domain/commission/model/commission-creator.go
+++ domain/commission/model/commission-creator.go
@@ -6,19 +6,23 @@ import (
 )
 
 type CommissionCreator struct {
-	OpenCommissionID string      `json:"openCommissionId"`
-	ArtistID         string      `json:"artistID"`
-	RequesterID      string      `json:"requesterID"`
-	Price            model.Price `json:"price"`
-	DayNeed          int         `json:"dayNeed"`
-	Size             *model.Size `json:"size"`
-	Resolution       *float64    `json:"resolution"`
-	ExportFormat     *string     `json:"exportFormat"`
-	Desc             string      `json:"desc"`
-	PaymentMethod    string      `json:"paymentMethod"`
-	IsR18            bool        `json:"isR18"`
-	BePrivate        bool        `json:"bePrivate"`
-	Anonymous        bool        `json:"anonymous"`
-	RefImages        []image.Image
-	RefImagePaths    []string `json:"refImagePaths"`
+	OpenCommissionID     string      `json:"openCommissionId"`
+	ArtistID             string      `json:"artistID"`
+	ArtistName           string      `json:"artistName"`
+	ArtistProfilePath    *string     `json:"artistProfilePath"`
+	RequesterID          string      `json:"requesterID"`
+	RequesterName        string      `json:"requesterName"`
+	RequesterProfilePath *string     `json:"requesterProfilePath"`
+	Price                model.Price `json:"price"`
+	DayNeed              int         `json:"dayNeed"`
+	Size                 *model.Size `json:"size"`
+	Resolution           *float64    `json:"resolution"`
+	ExportFormat         *string     `json:"exportFormat"`
+	Desc                 string      `json:"desc"`
+	PaymentMethod        string      `json:"paymentMethod"`
+	IsR18                bool        `json:"isR18"`
+	BePrivate            bool        `json:"bePrivate"`
+	Anonymous            bool        `json:"anonymous"`
+	RefImages            []image.Image
+	RefImagePaths        []string `json:"refImagePaths"`
 }
