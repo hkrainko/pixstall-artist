c1de7680a653cca060bd905bbbf2d6b18a8d16cb
hkrainko <>
2021-06-27 18:28:38 +0800

    no message

--- app/bookmark/delivery/http/ctrl.go
+++ app/bookmark/delivery/http/ctrl.go
@@ -4,8 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	add_bookmark "pixstall-artist/app/bookmark/delivery/http/resp/add-bookmark"
+	get_bookmarks "pixstall-artist/app/bookmark/delivery/http/resp/get-bookmarks"
 	http2 "pixstall-artist/app/error/http"
 	"pixstall-artist/domain/bookmark"
+	error2 "pixstall-artist/domain/error"
+	"strconv"
 )
 
 type BookmarkController struct {
@@ -32,6 +35,22 @@ func (b BookmarkController) AddBookmark(c *gin.Context) {
 
 func (b BookmarkController) GetBookmarks(c *gin.Context) {
 	tokenUserID := c.GetString("userId")
+	count, err := strconv.Atoi(c.Query("count"))
+	if err != nil {
+		c.JSON(http2.NewErrorResponse(error2.BadRequestError))
+		return
+	}
+	offset, err := strconv.Atoi(c.Query("offset"))
+	if err != nil {
+		c.JSON(http2.NewErrorResponse(error2.BadRequestError))
+		return
+	}
+	result, err := b.bookmarkUseCase.GetBookmarksForUser(c, tokenUserID, count, offset)
+	if err != nil {
+		c.AbortWithStatusJSON(http2.NewErrorResponse(err))
+		return
+	}
+	c.JSON(http.StatusOK, get_bookmarks.NewResponse(*result, count, offset))
 }
 
 func (b BookmarkController) DeleteBookmark(c *gin.Context) {
--- app/bookmark/delivery/http/resp/get-bookmarks/success_resp.go
+++ app/bookmark/delivery/http/resp/get-bookmarks/success_resp.go
@@ -1,1 +1,25 @@
 package get_bookmarks
+
+import (
+	get_artist "pixstall-artist/app/artist/delivery/model/get-artist"
+	domain "pixstall-artist/domain/artist/model"
+)
+
+type Response struct {
+	Artists []get_artist.Response `json:"artists"`
+	Count   int                   `json:"count"`
+	Offset  int                   `json:"offset"`
+}
+
+func NewResponse(dArtists []domain.Artist, count int, offset int) *Response {
+	rArtists := make([]get_artist.Response, 0)
+	for _, v := range dArtists {
+		rArtists = append(rArtists, *get_artist.NewResponse(v))
+	}
+
+	return &Response{
+		Artists: rArtists,
+		Count: count,
+		Offset: offset,
+	}
+}
